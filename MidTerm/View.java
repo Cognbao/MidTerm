2adfa20554c785e4d6a7f62acd2a5cdf666a1cd4
Cognbao <161429486+Cognbao@users.noreply.github.com>
2024-04-04 22:26:23 +0700

    Add files via upload

---
+++ MidTerm/View.java
@@ -0,0 +1,12 @@
+package MidTerm;
+
+import java.util.*;
+
+// View
+public class View {
+    public void displayLines(List<String> lines) {
+        for (String line : lines) {
+            System.out.println(line);
+        }
+    }
+}
